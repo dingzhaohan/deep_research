51260ec359a013a7bf9efc3c4ed63e04ac33cf9c
dingzhaohan <dingzhaohan@pku.edu.cn>
2020-01-16 17:38:39 +0800

    fix bug

--- spiders/spider/spider/spiders/big.py
+++ spiders/spider/spider/spiders/big.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-import scrapy
-import pandas as pd
-df = pd.read_json('/home/zhaohan/Desktop/research/links-between-papers-and-code.json')
-
-class BigSpider(scrapy.Spider):
-    name = 'big'
-    allowed_domains = ['github.com', 'paperswithcode.com']
-    start_urls = []
-
-    def parse(self, response):
-        pass
-
 import scrapy
 import pandas as pd
 import urllib
