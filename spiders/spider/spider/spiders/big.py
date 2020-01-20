0c68f225baa796ef3863bb4acd5f4511e43eed1d
dingzhaohan <dingzhaohan@pku.edu.cn>
2020-01-20 09:45:47 +0800

    write scrapy code

--- spiders/spider/spider/spiders/big.py
+++ spiders/spider/spider/spiders/big.py
@@ -62,7 +62,78 @@ class BigSpider(scrapy.Spider):
         return item
 
     def parse_git(self, response):
-        pass
+
+        index = self.start_urls.index(response.url)
+
+        item = GitItem()
+
+        sites = json.loads(response.body_as_unicode())
+
+        item["paper_title"] = df["paper_title"][index]
+
+        item["repo_size"] = sites["size"]
+
+        # item["repo_name"] = sites["name"]
+
+        item["repo_url"] = sites["html_url"]
+
+        item["subscribe_count"] = sites["subscribers_count"]
+
+        item["forks_count"] = sites["forks_count"]
+
+        item["star_counts"] = sites["stargazers_count"]
+
+        item["repo_created_at"] = sites["created_at"][:10]
+
+        item["repo_updated_at"] = sites["updated_at"][:10]
+
+        item["repo_kept_time"] = caltime(item["repo_created_at"], item["repo_updated_at"])
+
+        item["open_issues_count"] = sites["open_issues_count"]
+
+        url1 = response.url.replace('?client_id=a26c83afeb1a41304d10&client_secret=ea8586a6b1d16c9f645112fd04b5bf57f5bae88e','/issues?client_id=a26c83afeb1a41304d10&client_secret=ea8586a6b1d16c9f645112fd04b5bf57f5bae88e')
+
+        yield scrapy.Request(url1, meta={"item":item}, callback=self.detail_parse1)
+
+    def parse_git_readme(self, response):
+
+        sites = json.loads(response.body_as_unicode())
+        item = response.meta["item"]
+        try:
+            item["readme_size"] = sites["size"]
+        except:
+            url = response.url.replace("README", "readme")
+            yield scrapy.Request(url, meta={"item":response.meta["item"]}, callback=self.detail_parse2)
+        return item
+
+    def parse_git_issues(self, response):
+        
+        sites = json.loads(response.body_as_unicode())
+        item = response.meta["item"]
+        try:
+            item["latest_issues_created_at"] = sites[0]["created_at"][:10]
+        except:
+            item["latest_issues_created_at"] = None
+        try:
+            item["latest_issues_updated_at"] = sites[0]["updated_at"][:10]
+        except:
+            item["latest_issues_updated_at"] = None
+        print(response.url)
 
     def parse_ssr(self, response):
-        pass
+        item = SementicItem()
+        json_data = json.loads(response.body_as_unicode())
+        item["abstract"] = json_data["abstract"]
+        item["arxivId"] = json_data["arxivId"]
+        item["authors"] = json_data["authors"]
+        item["citationVelocity"] = json_data["citationVelocity"]
+        item["citations"] = json_data["citations"]
+        item["doi"] = json_data["doi"]
+        item["influentialCitationCount"] = json_data["influentialCitationCount"]
+        item["references"] = json_data["references"]
+        item["title"] = json_data["title"]
+        item["topics"] = json_data["topics"]
+        item["url"] = json_data["url"]
+        item["venue"] = json_data["venue"]
+        item["year"] = json_data["year"]
+        yield item
--- spiders/ssr/ssr/spiders/littlessr.py
+++ spiders/ssr/ssr/spiders/littlessr.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
-import scrapy
 
+import pandas as pd
+import scrapy
+import json
+df = pd.read_json('/Users/zhaohan/Desktop/Grade3/本研/json_data/papers_with_abstracts.json')
 
 class LittlessrSpider(scrapy.Spider):
     name = 'littlessr'
-    allowed_domains = ['semantic.scholar.com']
-    start_urls = ['http://semantic.scholar.com/']
+    allowed_domains = ['arxiv.org']
+    start_urls = []
+
+    for i in range(len(df)):
+        if df["arxiv_id"][i]:
+            start_urls.append('https://api.semanticscholar.org/v1/paper/arXiv:' + str(df["arxiv_id"][i]))
 
     def parse(self, response):
-        pass
+        item = SementicItem()
+        json_data = json.loads(response.body_as_unicode())
+        item["abstract"] = json_data["abstract"]
+        item["arxivId"] = json_data["arxivId"]
+        item["authors"] = json_data["authors"]
+        item["citationVelocity"] = json_data["citationVelocity"]
+        item["citations"] = json_data["citations"]
+        item["doi"] = json_data["doi"]
+        item["influentialCitationCount"] = json_data["influentialCitationCount"]
+        item["references"] = json_data["references"]
+        item["title"] = json_data["title"]
+        item["topics"] = json_data["topics"]
+        item["url"] = json_data["url"]
+        item["venue"] = json_data["venue"]
+        item["year"] = json_data["year"]
+        yield item
